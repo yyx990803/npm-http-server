21830729ebd3da8be58eb336fa7b110adc363bc8
Michael Jackson <mjijackson@gmail.com>
2015-11-08 10:15:14 -0800

    Add support for scoped packages
    Fixes #2

--- modules/getPackageInfo.js
+++ modules/getPackageInfo.js
@@ -1,10 +1,23 @@
-import RegistryClient from 'silent-npm-registry-client'
+import { get } from 'request'
 
 const RegistryURL = process.env.npm_package_config_registryURL
-const client = new RegistryClient
 
 function getPackageInfo(packageName, callback) {
-  client.get(`${RegistryURL}/${packageName}`, { timeout: 1000 }, callback)
+  var encodedPackageName
+  if (packageName.charAt(0) === '@') {
+    encodedPackageName = `@${encodeURIComponent(packageName.substring(1))}`
+  } else {
+    encodedPackageName = encodeURIComponent(packageName)
+  }
+
+  get({
+    uri: `${RegistryURL}/${encodedPackageName}`,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }, function (error, res) {
+    callback(error, res && res.body ? JSON.parse(res.body) : null)
+  })
 }
 
 export default getPackageInfo
