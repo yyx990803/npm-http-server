b736d13cc542f711f3f4f21715de160ef86b9685
Michael Jackson <mjijackson@gmail.com>
2015-11-09 09:11:07 -0800

    Use LRU cache to avoid hitting npm registry
    Fixes #7

--- modules/getPackageInfo.js
+++ modules/getPackageInfo.js
@@ -1,23 +1,42 @@
 import { get } from 'request'
+import LRU from 'lru-cache'
 
 const RegistryURL = process.env.npm_package_config_registryURL
 
+const OneMinute = 60 * 1000
+const RegistryCache = LRU({
+  max: 500,
+  maxAge: OneMinute
+})
+
 function getPackageInfo(packageName, callback) {
-  var encodedPackageName
+  let encodedPackageName
   if (packageName.charAt(0) === '@') {
     encodedPackageName = `@${encodeURIComponent(packageName.substring(1))}`
   } else {
     encodedPackageName = encodeURIComponent(packageName)
   }
 
-  get({
-    uri: `${RegistryURL}/${encodedPackageName}`,
-    headers: {
-      'Accept': 'application/json'
-    }
-  }, function (error, res) {
-    callback(error, res && res.body ? JSON.parse(res.body) : null)
-  })
+  let value = RegistryCache.get(encodedPackageName)
+
+  if (value) {
+    callback(null, value)
+  } else {
+    get({
+      uri: `${RegistryURL}/${encodedPackageName}`,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }, function (error, res) {
+      if (res && res.body) {
+        value = JSON.parse(res.body)
+        RegistryCache.set(encodedPackageName, value)
+        callback(null, value)
+      } else {
+        callback(error)
+      }
+    })
+  }
 }
 
 export default getPackageInfo
