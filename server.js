e93d6fb29bf134359b57c6173687d4fbcc93cdc7
Michael Jackson <mjijackson@gmail.com>
2015-11-09 16:46:43 -0800

    Use express.static to serve website

--- server.js
+++ server.js
@@ -2,14 +2,13 @@
 require('babel/register')
 
 var express = require('express')
-var serveWebsite = require('./modules/serveWebsite')
 var serveNPMPackageFile = require('./modules/serveNPMPackageFile')
 
 var port = process.env.PORT || process.env.npm_package_config_port
 var app = express()
 
 app.disable('x-powered-by')
-app.use(serveWebsite)
+app.use(express.static('public', { maxAge: 60000 }))
 app.use(serveNPMPackageFile)
 
 app.listen(port, function () {
