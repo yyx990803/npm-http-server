c42a910994f6033617b6dd97fe7369993e5e08c9
Michael Jackson <mjijackson@gmail.com>
2016-02-21 17:30:44 -0800

    Update example server

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 /*eslint-disable no-console */
 require('babel-core/register')
 
-var express = require('express')
-var serveNPMPackageFile = require('./modules/serveNPMPackageFile').default
+var port = process.env.PORT || 5000
 
-var port = process.env.PORT || process.env.npm_package_config_port
+var express = require('express')
+var createRequestHandler = require('./modules').createRequestHandler
+
 var app = express()
 var cors = require('cors')
 
 app.disable('x-powered-by')
 app.use(cors())
 app.use(express.static('public', { maxAge: 60000 }))
-app.use(serveNPMPackageFile)
+app.use(
+  createRequestHandler()
+)
 
 app.listen(port, function () {
   console.log('Server started on port ' + port + '. Ctrl+C to quit')
