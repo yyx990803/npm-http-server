81a0435e847e83964228b9de644a223a020c4859
Michael Jackson <mjijackson@gmail.com>
2016-04-12 02:46:05 -0700

    Style tweaks

--- modules/index.js
+++ modules/index.js
@@ -126,8 +126,8 @@ export const createRequestHandler = (options = {}) => {
           } else if (autoIndex) {
             statFile(filepath, (error, stats) => {
               if (stats && stats.isDirectory()) {
-                // Append `/` to directory urls
-                if(req.url[req.url.length - 1] !== '/') {
+                // Append `/` to directory URLs
+                if (req.url[req.url.length - 1] !== '/') {
                   sendRedirect(res, req.url + '/', redirectTTL)
                 } else {
                   generateDirectoryIndexHTML(tarballDir, filename, displayName, (error, html) => {
