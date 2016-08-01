f8a514440847300ca49b42968672f4d4668e80d2
Michael Jackson <mjijackson@gmail.com>
2016-08-01 14:55:07 -0700

    Call the callback (duh)

--- modules/RegistryUtils.js
+++ modules/RegistryUtils.js
@@ -49,7 +49,7 @@ export const getPackageInfo = (registryURL, packageName, callback) => {
     info => callback(null, info),
     error => {
       RegistryCache.del(cacheKey)
-      throw error
+      callback(error)
     }
   )
 }
